a62ffc0c9fd365ed9b5441422f880abbfb1fa103
joy43 <ssjoy43@gmail.com>
2023-12-22 12:18:30 +0600

    sucess mongodb connect

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+// const axios = require('axios');
+const app = express();
+const cors = require('cors');
+
+//  require('dotenv').config();
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+// require('crypto').randomBytes(64).toString('hex')
+const port = process.env.PORT || 5000;
+
+// ---------------middleware--------------
+app.use(cors());
+app.use(express.json());
 
 
 
@@ -6,8 +20,7 @@
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://task:<password>@taskmanage.cx6sida.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://task:yd6llOUC9vhRDHGQ@taskmanage.cx6sida.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,13 +34,23 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+
+    // -------------------connect mogodb---------------------------------------------
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    
   }
 }
 run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('tech is sitting on')
+  })
+  
+  app.listen(port, () => {
+    console.log(`tech is sitting on port ${port}`);
+  })
